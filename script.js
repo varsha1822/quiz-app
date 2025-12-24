68a862b9c24e134dd4c7dda60b05f3980ebff033
varsha1822 <varshadushpakula@gmail.com>
2025-12-24 14:02:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -26,60 +26,85 @@ const quizData = [
 ];
 
 const questionEl = document.getElementById("question");
-const answerEls = document.querySelectorAll(".answer");
+const answersEls = document.querySelectorAll(".answer");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
 let currentQuiz = 0;
 let score = 0;
+let userAnswers = [];
 
 loadQuiz();
 
 function loadQuiz() {
   deselectAnswers();
 
-  const currentQuizData = quizData[currentQuiz];
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
+  const q = quizData[currentQuiz];
+  questionEl.innerText = q.question;
+  a_text.innerText = q.a;
+  b_text.innerText = q.b;
+  c_text.innerText = q.c;
+  d_text.innerText = q.d;
+
+  if (userAnswers[currentQuiz]) {
+    document.getElementById(userAnswers[currentQuiz]).checked = true;
+  }
+
+  prevBtn.disabled = currentQuiz === 0;
+
+  if (currentQuiz === quizData.length - 1) {
+    nextBtn.innerText = "Submit";
+  } else {
+    nextBtn.innerText = "Next";
+  }
 }
 
 function deselectAnswers() {
-  answerEls.forEach(answer => answer.checked = false);
+  answersEls.forEach(el => el.checked = false);
 }
 
 function getSelected() {
   let answer;
-  answerEls.forEach(answerEl => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
-    }
+  answersEls.forEach(el => {
+    if (el.checked) answer = el.id;
   });
   return answer;
 }
 
-submitBtn.addEventListener("click", () => {
+nextBtn.addEventListener("click", () => {
   const answer = getSelected();
+  if (!answer) return;
 
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
+  userAnswers[currentQuiz] = answer;
+
+  if (currentQuiz === quizData.length - 1) {
+    calculateScore();
+    return;
+  }
+
+  currentQuiz++;
+  loadQuiz();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentQuiz--;
+  loadQuiz();
+});
+
+function calculateScore() {
+  score = 0;
+  quizData.forEach((q, index) => {
+    if (q.correct === userAnswers[index]) {
       score++;
     }
+  });
 
-    currentQuiz++;
-
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
-    } else {
-      document.querySelector(".quiz-container").innerHTML = `
-        <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-        <button onclick="location.reload()">Reload</button>
-      `;
-    }
-  }
-});
+  document.querySelector(".quiz-container").innerHTML = `
+    <h2>You scored ${score} / ${quizData.length}</h2>
+    <button onclick="location.reload()">Restart</button>
+  `;
+}
